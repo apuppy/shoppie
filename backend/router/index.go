fe836f9ac42fbfd5313971c2d0b4ffe7b32b0455
lihongde <lihongde@leweikeji.com>
2020-03-21 22:22:11 +0800

    add site home route

--- backend/router/index.go
+++ backend/router/index.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"net/http"
 	"shoppie/user"
 
 	"github.com/gin-gonic/gin"
@@ -11,7 +12,14 @@ func Start() {
 
 	r := gin.Default()
 
+	r.GET("/", siteHome)
 	r.GET("/user", user.Home)
 
 	r.Run()
+}
+
+func siteHome(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Welcome to shoppie.",
+	})
 }
