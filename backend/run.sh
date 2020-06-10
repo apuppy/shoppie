05eba84d332523c4e74fc8be224874ed86c33f31
lihongde <lihongde@leweikeji.com>
2020-06-10 15:54:11 +0800

    docker support

---
+++ backend/run.sh
@@ -0,0 +1,4 @@
+# build shoppie image
+docker build -t shoppie .
+# launch shoppie backend service
+docker run -d -p 8090:8080 -v $PWD/config/settings.yml:/root/config/settings.yml --rm -it --name shoppie-backend shoppie
